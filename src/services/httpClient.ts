51eb28ea45a28c3c22622926ecd1c331b084abc9
AbubakirMahkamov-prog <mahkamovabubakir65119@gmail.com>
2024-02-23 23:15:10 +0500

    f

--- src/services/httpClient.ts
+++ src/services/httpClient.ts
@@ -46,7 +46,7 @@ axiosInstance.interceptors.request.use(async req => {
 axiosInstance.interceptors.response.use(function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
-    console.log(response.headers)
+    // console.log(response.)
     return response;
 }, function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
